8b785123d5084c33684ab5843d32caf302a3325d
John Vincent Uy Calacsan <johnvuc@hotmail.com>
2017-11-21 11:17:42 +0100

    Update v1.0.1

--- designs.js
+++ designs.js
@@ -5,24 +5,21 @@
 var button = "yo";
 var color = $('#colorPicker'); 
 
-function makeGrid() {
+var column = $('#input_height').val();
+var row =  $('#input_width').val();
+var color = $('#colorPicker').val();
+var onGrid = 0;
+
+// Grid Creation function
+
+function makeGrid(gridHeight,gridWidth) {
 // Your code goes here!
 
 var table = $('#pixel_canvas');
-var column = 30;
-var row = 30;
 
-
-
-
-$('tbody').children().append('<td></td>');
-
-var column
-var row
-
-for(let i = 0; i < column; i++) {
+for(let i = 0; i < gridHeight; i++) {
 		table.append('<tr id="grid-'+i+'"></tr>');
-		for( let j = 0; j < row ; j++) {
+		for( let j = 0; j < gridWidth ; j++) {
 			$('tbody').children('#grid-'+""+i+"").append('<td id="grid-'+i+'-'+j+'"></td>');
 
 		}
@@ -32,6 +29,50 @@ for(let i = 0; i < column; i++) {
 
 console.log();
 
-$(makeGrid());
+// Grid Values
+$('#input_height').on('change', function(){
+
+	column = $('#input_height').val();
+
+});
+
+$('#input_width').on('change', function(){
+
+	row = $('#input_width').val();
+
+});
+// COLORS 
+$('#colorPicker').on('change',function(){
+	color = $('#colorPicker').val();
+});
+
+
+// Grid Box, on Click change background color
+$('body').on('click','td', function(){
+
+	$(this).css('background-color', color);
+
+});
+
+// On click Grid Appears
+
+$('#submit').on('click', function(){
+
+do {
+	onGrid = 0;
+	$('tbody').remove();
+
+} while(onGrid === 1) {
+
+	makeGrid(column, row);
+	return false;
+	onGrid = 1;
+}
+
+
+
+});
+
+// TESTS 
 
 
