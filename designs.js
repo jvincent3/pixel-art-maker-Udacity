908c283481c7ad7ea951e9a4874f245b687755b7
John Vincent Uy Calacsan <johnvuc@hotmail.com>
2018-01-04 20:17:04 +0100

    Cleaned the code
    Cleaned the code and Added color on hover+click functionality

--- designs.js
+++ designs.js
@@ -1,97 +1,88 @@
-(function(){
+// When size is submitted by the user, call makeGrid()
+const body = $('body');
 
-	var grid = {
 
-		init: function(){
-			this.cacheDom();		
-		//	this.test();			
-			this.inputUpdate();
-			this.gridUpdate();
-			this.colorUpdate();
-			this.addColor();
-		},
-		cacheDom: function() {
-			this.$el = $('#sizePicker');
-			this.$input = this.$el.find('input');
-			this.$button = this.$el.find('#submit');
-			this.table = $('#pixel_canvas');
-			this.tbody = $('tbody');
-			this.pickColor = $('#colorPicker');
-			this.$body = $('body');
+var column = $('#input_height').val();
+var row =  $('#input_width').val();
+var colorPicker = $('#colorPicker')
+var onGrid = 0;
 
-		},
 
-		render: function(){
-			
-			valueColumn = $('#input_height').val();
-			this.addGrid();
-		},
-		test: function(){
 
-			this.$input.on('change', function(){
-			valueColumn = $('#input_height').val();
-			
-			});
-		},
+// Grid Creation function
+function makeGrid(gridHeight,gridWidth) {
 
-		// CREATE GRID BOX
-			// FUNCTION TO CREATE THE ROWS x COLUMNS
-		addGrid: function(valColumn, valRow){
-			
-			for(let i = 0; i < valColumn; i++) {
-				this.table.append('<tr id="grid-'+i+'"></tr>');
-				for( let j = 0; j < valRow ; j++) {
-					$('tbody').children('#grid-' + "" + i + "").append('<td id="grid-' + i + '-' + j + '"></td>');
-				}
+	var table = $('#pixel_canvas');
+
+	for(let i = 0; i < gridHeight; i++) {
+			table.append('<tr id="grid-'+i+'"></tr>');
+			for( let j = 0; j < gridWidth ; j++) {
+				$('tbody').children('#grid-' + "" + i + "").append('<td id="grid-' + i + '-' + j + '"></td>');
 			}
-		},
-			//CALL AND RESENTS THE DRAWING SHEET
-		gridUpdate: function(valColumn, valRow){
+		}
+	}
 
-			this.$button.on('click', function(){
-				var onGrid = 0;
-				do {
-					$('tbody').remove();
-					ongrid = 0;
-				} while(onGrid === 1) {					
-				grid.addGrid(valColumn, valRow);	
-				grid.addColor(grid.pickColor.val());			
-				return false;
-				ongrid = 1;
-				}				
-			});
+// Grid Values
+	$('#input_height').on('change', function(){
+
+		column = $('#input_height').val();
+
+
+	});
+
+	$('#input_width').on('change', function(){
+
+		row = $('#input_width').val();
+
+	});
+// COLOR Picker
+	function colorBox(element){  
+        
+     	let color = colorPicker.val();
+        
+        $(element).css("background-color", color);
+        
+    }
+
+// Grid Box, on Click change background color
+	body.on('mousedown','td', function(){
+
+		//$(this).css('background-color', color);
+		colorBox(this);
+	});
+
+// Add color on hover and left mouse is clicked
+    body.on("mouseover", "td", function(e){
+        
+ 		if(e.buttons == 1){
+        
+            colorBox(this);
+        
+        }
+        
+    });
+
 			
-		},
-		
-		inputUpdate: function(){
 
-			this.$input.on('change', function(){
-				var valColumn = $('#input_height').val();
-				var valRow = $('#input_width').val();
-
-				grid.gridUpdate(valColumn, valRow);
-			});
-		},
+// On click Grid Appears
 
 
-		// COLORING
-		colorUpdate: function(){
+	$('#submit').on('click', function(){
 
-			this.pickColor.on('change', function(){
-				color = $('#colorPicker').val();
-				grid.addColor(color);
-			});			
-		},
+	do {
+		onGrid = 0;
+		$('tbody').remove();
+	} while(onGrid === 1) {
 
-		addColor: function(color){
-			this.$body.on('mousedown', 'td', function(){
-				$(this).css('background-color', color);
-			})
-		},
-	};
+		makeGrid(column, row);
+		return false;
+		onGrid = 1;
 
-	grid.init();
+	}
 
-})()
+
+
+	});
+
 
 
